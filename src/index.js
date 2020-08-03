8c5538d409e3ca020a05881f91554319bd00564a
ruicarest <ruicarest@gmail.com>
2020-08-03 23:45:17 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,264 @@
+import "./styles.css";
+
+const TILES_NUMBER = 400;
+const TILES_WIDTH = 40;
+const BUILDINGS_NUMBER = 5;
+
+const TILETYPES = {
+  untouched: 0,
+  road: 1,
+  building: 2,
+  tree: 3,
+  water: 4,
+};
+
+var mapTilesArray = [...Array(TILES_NUMBER)].map(() => TILETYPES.untouched);
+
+var buildingIndexes = [];
+
+function testAspectRatio() {
+  return TILES_NUMBER % TILES_WIDTH != 0 ? false : true;
+}
+
+function isFirstOnLine(index) {
+  return index % TILES_WIDTH == 0 ? true : false;
+}
+
+function isLastOnLine(index) {
+  return index % TILES_WIDTH == TILES_WIDTH - 1 ? true : false;
+}
+
+function isInFirstLine(index) {
+  return index - TILES_WIDTH < 0 ? true : false;
+}
+
+function isInLastLine(index) {
+  return index + TILES_WIDTH >= TILES_NUMBER ? true : false;
+}
+
+// 0 1 2
+// 3 4 5
+// 6 7 8
+function getNeighboursTiles(index) {
+  let neighboursTiles = [9];
+
+  if (isFirstOnLine(index)) {
+    neighboursTiles[0] = -1;
+    neighboursTiles[3] = -1;
+    neighboursTiles[6] = -1;
+  } else if (isLastOnLine(index)) {
+    neighboursTiles[2] = -1;
+    neighboursTiles[5] = -1;
+    neighboursTiles[8] = -1;
+  }
+
+  if (isInFirstLine(index)) {
+    neighboursTiles[0] = -1;
+    neighboursTiles[1] = -1;
+    neighboursTiles[2] = -1;
+  } else if (isInLastLine(index)) {
+    neighboursTiles[6] = -1;
+    neighboursTiles[7] = -1;
+    neighboursTiles[8] = -1;
+  }
+
+  neighboursTiles[0] =
+    neighboursTiles[0] != -1 ? mapTilesArray[index - TILES_WIDTH - 1] : -1;
+  neighboursTiles[1] =
+    neighboursTiles[1] != -1 ? mapTilesArray[index - TILES_WIDTH] : -1;
+  neighboursTiles[2] =
+    neighboursTiles[2] != -1 ? mapTilesArray[index - TILES_WIDTH + 1] : -1;
+  neighboursTiles[3] = neighboursTiles[3] != -1 ? mapTilesArray[index - 1] : -1;
+  neighboursTiles[4] = mapTilesArray[index];
+  neighboursTiles[5] = neighboursTiles[5] != -1 ? mapTilesArray[index + 1] : -1;
+  neighboursTiles[6] =
+    neighboursTiles[6] != -1 ? mapTilesArray[index + TILES_WIDTH - 1] : -1;
+  neighboursTiles[7] =
+    neighboursTiles[7] != -1 ? mapTilesArray[index + TILES_WIDTH] : -1;
+  neighboursTiles[8] =
+    neighboursTiles[8] != -1 ? mapTilesArray[index + TILES_WIDTH + 1] : -1;
+
+  return neighboursTiles;
+}
+
+// 0 1 2
+// 3 4 5
+// 6 7 8
+function getNeighboursIndexes(index) {
+  let neighboursIndexes = [9];
+
+  if (isFirstOnLine(index)) {
+    neighboursIndexes[0] = -1;
+    neighboursIndexes[3] = -1;
+    neighboursIndexes[6] = -1;
+  } else if (isLastOnLine(index)) {
+    neighboursIndexes[2] = -1;
+    neighboursIndexes[5] = -1;
+    neighboursIndexes[8] = -1;
+  }
+
+  if (isInFirstLine(index)) {
+    neighboursIndexes[0] = -1;
+    neighboursIndexes[1] = -1;
+    neighboursIndexes[2] = -1;
+  } else if (isInLastLine(index)) {
+    neighboursIndexes[6] = -1;
+    neighboursIndexes[7] = -1;
+    neighboursIndexes[8] = -1;
+  }
+
+  neighboursIndexes[0] =
+    neighboursIndexes[0] != -1 ? index - TILES_WIDTH - 1 : -1;
+  neighboursIndexes[1] = neighboursIndexes[1] != -1 ? index - TILES_WIDTH : -1;
+  neighboursIndexes[2] =
+    neighboursIndexes[2] != -1 ? index - TILES_WIDTH + 1 : -1;
+  neighboursIndexes[3] = neighboursIndexes[3] != -1 ? index - 1 : -1;
+  neighboursIndexes[4] = index;
+  neighboursIndexes[5] = neighboursIndexes[5] != -1 ? index + 1 : -1;
+  neighboursIndexes[6] =
+    neighboursIndexes[6] != -1 ? index + TILES_WIDTH - 1 : -1;
+  neighboursIndexes[7] = neighboursIndexes[7] != -1 ? index + TILES_WIDTH : -1;
+  neighboursIndexes[8] =
+    neighboursIndexes[8] != -1 ? index + TILES_WIDTH + 1 : -1;
+
+  return neighboursIndexes;
+}
+
+function getTilePositionVertical(index) {
+  return Math.floor(index / TILES_WIDTH);
+}
+
+function getTilePositionHorizontal(index) {
+  return Math.floor(index % TILES_WIDTH);
+}
+
+function drawRoad(start, end) {
+  var startPositionVertical = getTilePositionVertical(start);
+  var startPositionHorizontal = getTilePositionHorizontal(start);
+  var endPositionVertical = getTilePositionVertical(end);
+  var endPositionHorizontal = getTilePositionHorizontal(end);
+
+  var nextPost = start;
+
+  if (startPositionVertical == endPositionVertical) {
+    if (startPositionHorizontal < endPositionHorizontal) {
+      nextPost = start + 1;
+    } else if (startPositionHorizontal > endPositionHorizontal) {
+      nextPost = start - 1;
+    }
+  } else if (startPositionVertical < endPositionVertical) {
+    nextPost = start + TILES_WIDTH;
+  } else {
+    nextPost = start - TILES_WIDTH;
+  }
+
+  if (nextPost == end || mapTilesArray[nextPost] == TILETYPES.road) {
+    return true;
+  }
+
+  mapTilesArray[nextPost] =
+    mapTilesArray[nextPost] != 0 ? mapTilesArray[nextPost] : TILETYPES.road;
+
+  return drawRoad(nextPost, end);
+}
+
+function drawBuildings(buildingNumber) {
+  for (let i = 0; i < buildingNumber; i++) {
+    let newLocation = -1;
+    while (newLocation === -1) {
+      newLocation = Math.floor(Math.random() * TILES_NUMBER);
+      let buildingTiles = getNeighboursTiles(newLocation);
+
+      if (!buildingTiles.find((value) => value > 0)) {
+        getNeighboursIndexes(newLocation).forEach(
+          (value) => (mapTilesArray[value] = TILETYPES.building)
+        );
+
+        buildingIndexes.push(newLocation);
+      } else {
+        newLocation = -1;
+      }
+    }
+  }
+}
+
+function drawRoadsConnectingBuildings(count = 0) {
+  if (count == buildingIndexes.length) {
+    console.log(count);
+    return;
+  }
+
+  drawRoad(
+    buildingIndexes[count],
+    buildingIndexes[Math.floor(Math.random() * buildingIndexes.length)]
+  );
+
+  drawRoadsConnectingBuildings(count + 1);
+}
+
+function drawBuildingsWithRoads(count, isFirst = true) {
+  //console.log('terceiro');
+
+  if (count == 0) return;
+
+  if (isFirst) {
+    buildingIndexes = [];
+  }
+
+  let newLocation = Math.floor(Math.random() * TILES_NUMBER);
+
+  while (mapTilesArray[newLocation] != 0) {
+    newLocation = Math.floor(Math.random() * TILES_NUMBER);
+  }
+
+  mapTilesArray[newLocation] = 2;
+
+  if (!isFirst) {
+    drawRoad(
+      newLocation,
+      buildingIndexes[Math.floor(Math.random() * buildingIndexes.length)]
+    );
+  }
+
+  buildingIndexes.push(newLocation);
+
+  drawBuildingsWithRoads(count - 1, false);
+}
+
+function writeOnCanvas(tilesArray = mapTilesArray, isError = false) {
+  if (isError) {
+    document.querySelector("#canvas").innerHTML = tilesArray;
+    return;
+  }
+
+  let element = document.querySelector("#canvas");
+  //let tiles = tilesArray.reduce((acc, numb) => {return acc + "" + numb});
+
+  let tiles = tilesArray.reduce((acc, numb, index) => {
+    return index % TILES_WIDTH == 0 ? acc + "<br>" + numb : acc + "" + numb;
+  });
+
+  element.innerHTML = tiles;
+}
+
+const isTestOk = testAspectRatio();
+
+if (isTestOk) {
+  setInterval(function () {
+    mapTilesArray = [...Array(TILES_NUMBER)].map(
+      (value) => TILETYPES.untouched
+    );
+    drawBuildingsWithRoads(BUILDINGS_NUMBER);
+    writeOnCanvas();
+  }, 1000);
+
+  //drawBuildings(BUILDINGS_NUMBER);
+  //drawRoadsConnectingBuildings();
+
+  //writeOnCanvas();
+
+  console.log(getNeighboursTiles(Math.floor(Math.random() * TILES_NUMBER)));
+  console.log(getNeighboursIndexes(Math.floor(Math.random() * TILES_NUMBER)));
+} else {
+  writeOnCanvas("isTestOk failed", true);
+}
