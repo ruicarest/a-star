421ad1aae65720a452aabd983802e42e1a7a0dd3
ruicarest <ruicarest@gmail.com>
2020-08-30 18:37:38 +0100

    new group manager

--- src/group-manager.js
+++ src/group-manager.js
@@ -10,186 +10,135 @@ draw on that one
 if tile has 8 neibours of same tile type marked as "surrounded" and skip
 */
 
-/* add boundary nodes to a "bag" with a group node value 
-  draw the tile on the node with the highest score
-*/
-
-export function drawGroupTiles2(Type, poolSize) {
+export function drawGroupTiles(Type, poolSize) {
   var groupTilesArray = [];
+  var possibleNextNodes = [];
   var count = poolSize;
   var nodeType = Type;
+  var current;
 
-  var current = getRandomNode(WORLD_INFO.WorldNodesMatrix);
+  var WorldCopy = [...WORLD_INFO.WorldNodesMatrix];
 
   findNextGroupTile();
 
   function findNextGroupTile(safeTry = 10) {
-    if (current.tileType !== nodeType) {
+    //INFINITE LOOP SAFETY CHECK
+    if (safeTry == 1) {
+      console.log("Safety - couldn't find all the tiles");
+      return groupTilesArray;
+    }
+
+    if (count < 1) {
+      return;
+    }
+
+    //FIRST NODE
+    if (!current) {
+      current = getRandomNode(WORLD_INFO.WorldNodesMatrix);
+
+      if (
+        current.tileType !== TILETYPES.untouched ||
+        getValidNeighborNodesScore(current) < 3
+      ) {
+        current = false;
+        return findNextGroupTile(safeTry - 1);
+      }
+
       groupTilesArray.push(current);
-      //BUG: pushing building nodes and turning them a group tile
+
       WORLD_INFO.WorldNodesMatrix[current.pos.y][
         current.pos.x
       ].tileType = nodeType;
+      count--;
+      return findNextGroupTile();
     }
 
-    if (count == 1) {
-      return groupTilesArray;
-    }
-
-    //Remove intruders
-    var neighborNodes = getNeighborNodes(current).filter(
-      (node) =>
-        node !== -1 &&
-        node.pos !== current.pos &&
-        node.tileType !== nodeType &&
-        node.tileType !== TILETYPES.building
-    );
-
-    //No neighborNode undiscovered left
-    if (!Array.isArray(neighborNodes) || !neighborNodes.length) {
-      if (groupTilesArray.length < 2) {
-        current = getRandomNode(WORLD_INFO.WorldNodesMatrix);
-      } else {
-        current =
-          groupTilesArray[Math.floor(Math.random() * groupTilesArray.length)];
-      }
-
-      if (!current) {
-        console.log(
-          "Warning next known node: ",
-          current,
-          groupTilesArray,
-          count
-        );
-      }
-
-      return findNextGroupTile(safeTry - 1);
-    }
-
-    neighborNodes.map((neighborNode) => {
-      let neighborNodeNeighbours = getNeighborNodes(neighborNode);
-
-      //Remove intruders
-      let countSameTypeNeighbours = neighborNodeNeighbours.filter(
-        (node) =>
-          node !== -1 &&
-          node.tileType == nodeType &&
-          node.pos !== neighborNode.pos
-      ).length;
-
-      //lets get the one with highest score
-      neighborNode.sameTypeNeighbours = Math.max(
-        countSameTypeNeighbours,
-        neighborNode.sameTypeNeighbours ? neighborNode.sameTypeNeighbours : 0
-      );
+    groupTilesArray.forEach((groupNode, index, array) => {
+      getValidNeighborNodes(groupNode).forEach((node) => {
+        node.validNeighborNodeScore = getValidNeighborNodesScore(node);
+        if (!possibleNextNodes.find((element) => element.pos == node.pos)) {
+          possibleNextNodes.push(node);
+        }
+      });
     });
 
-    current = neighborNodes.reduce((max = 0, node) =>
-      max.sameTypeNeighbours > node.sameTypeNeighbours ? max : node
-    );
+    current = getOrganizedTiles();
 
-    if (!current) {
-      console.log("Warning new node: ", current, groupTilesArray, count);
-    }
+    groupTilesArray.push(current);
 
-    //move to next water node
-    count--;
-    return findNextGroupTile();
-  }
-}
-
-export function drawGroupTiles(Type, poolSize) {
-  var groupTilesArray = [];
-  var count = poolSize;
-  var nodeType = Type;
-
-  var current = getRandomNode(WORLD_INFO.WorldNodesMatrix);
-
-  findNextGroupTile();
-
-  function findNextGroupTile(safeTry = 10) {
-    //safety
-    if (count == 1) {
-      console.log("safety activated in findNextGroupTile()");
-      return groupTilesArray;
-    }
-
-    if (current.tileType !== TILETYPES.untouched) {
-      current = getRandomNode(WORLD_INFO.WorldNodesMatrix);
-      return findNextGroupTile(count - 1);
-    }
-
-    //set tile node to group tiletype
     WORLD_INFO.WorldNodesMatrix[current.pos.y][
       current.pos.x
     ].tileType = nodeType;
-
-    getNeighborNodes(current).forEach((node, index, theArray) => {
-      if (node.tileType === nodeType) {
-        node.neighborValidNodes--;
-      }
-    });
-
-    var neighborNodes = getNeighborValidNodes(current);
-
-    //set neighbor valid nodes number
-    current.neighborValidNodes = neighborNodes.length;
-
-    //add it to the group
-    groupTilesArray.push(current);
-
-    //No neighborNode undiscovered left
-    if (!neighborNodes.length) {
-      if (groupTilesArray.length < 2) {
-        current = getRandomNode(WORLD_INFO.WorldNodesMatrix);
-      } else {
-        //get the node with highest number of nodes
-        current = groupTilesArray.reduce((prev, curr) =>
-          curr.neighborValidNodes < prev.neighborValidNodes &&
-          curr.neighborValidNodes != 0
-            ? curr
-            : prev
-        );
-      }
-
-      return findNextGroupTile(safeTry - 1);
-    }
-
-    //get the node with highest number of nodes
-    var highValueGroupTile = groupTilesArray.reduce((prev, curr) => {
-      return curr.neighborValidNodes > prev.neighborValidNodes ? curr : prev;
-    });
-
-    var highValueGroupTileNeighbours = getNeighborValidNodes(
-      highValueGroupTile
-    );
-
-    current =
-      highValueGroupTileNeighbours[
-        Math.floor(Math.random() * highValueGroupTileNeighbours.length)
-      ];
-
-    if (!current) {
-      console.log(
-        WORLD_INFO.WorldNodesMatrix,
-        getNeighborNodes(highValueGroupTile),
-        "Warning new node: ",
-        highValueGroupTileNeighbours,
-        highValueGroupTile,
-        current,
-        groupTilesArray,
-        count
-      );
-    }
-
-    //move to next water node
     count--;
     return findNextGroupTile();
   }
 
-  function getNeighborValidNodes(nodeToEvaluate) {
-    //get valid neighbours
-    var neighborNodes = getNeighborNodes(nodeToEvaluate).filter(
+  function getOrganizedTiles() {
+    let indexToRemove = 0;
+    let nextNode = -1;
+
+    nextNode = possibleNextNodes.reduce((prev, curr, index) => {
+      if (
+        curr.validNeighborNodeScore < prev.validNeighborNodeScore &&
+        curr.neighborValidNodes > 0
+      ) {
+        indexToRemove = index;
+        return curr;
+      } else {
+        return prev;
+      }
+    });
+
+    possibleNextNodes.splice(indexToRemove, 1);
+
+    return nextNode;
+  }
+
+  function getSpiderTiles() {
+    let index = Math.floor(Math.random() * possibleNextNodes.length);
+    let nextNode = possibleNextNodes[index];
+
+    possibleNextNodes.splice(index, 1);
+
+    return nextNode;
+  }
+
+  //TODO: Improve this over here, it is not a great method
+  function getUnorganizedTiles() {
+    let bagOfIndexes = [];
+    let nextNode = -1;
+
+    nextNode = possibleNextNodes.reduce((prev, curr) => {
+      if (
+        curr.validNeighborNodeScore < prev.validNeighborNodeScore &&
+        curr.neighborValidNodes > 0
+      ) {
+        return curr;
+      } else {
+        return prev;
+      }
+    });
+
+    possibleNextNodes.find((element, index) => {
+      if (element.validNeighborNodeScore == nextNode.validNeighborNodeScore) {
+        bagOfIndexes.push(index);
+        return true;
+      }
+      return false;
+    });
+
+    let randomMaxIndex =
+      bagOfIndexes[Math.floor(Math.random(bagOfIndexes.length))];
+
+    possibleNextNodes.splice(randomMaxIndex, 1);
+
+    return possibleNextNodes[randomMaxIndex];
+  }
+
+  // valid
+  function getValidNeighborNodes(nodeToEvaluate) {
+    //get invalid neighbours
+    var validNeighborNodes = getNeighborNodes(nodeToEvaluate).filter(
       (node) =>
         node !== -1 &&
         node.pos !== nodeToEvaluate.pos &&
@@ -197,12 +146,12 @@ export function drawGroupTiles(Type, poolSize) {
         node.tileType !== TILETYPES.building
     );
 
-    return neighborNodes;
+    return validNeighborNodes;
   }
 
-  function getNeighborGroupScore(nodeToEvaluate) {
+  function getInvalidNeighborNodes(nodeToEvaluate) {
     //get valid neighbours
-    var neighborNodes = getNeighborNodes(nodeToEvaluate).filter(
+    var invalidNeighborNodes = getNeighborNodes(nodeToEvaluate).filter(
       (node) =>
         node === -1 ||
         node.pos === nodeToEvaluate.pos ||
@@ -210,6 +159,13 @@ export function drawGroupTiles(Type, poolSize) {
         node.tileType === TILETYPES.building
     );
 
-    return neighborNodes;
+    return invalidNeighborNodes;
+  }
+
+  function getValidNeighborNodesScore(nodeToEvaluate) {
+    let validScore = getValidNeighborNodes(nodeToEvaluate).length;
+    //let invalidScore = getNeighborInvalidNodes(nodeToEvaluate).length;
+
+    return validScore;
   }
 }
